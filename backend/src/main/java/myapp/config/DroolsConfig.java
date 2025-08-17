2665eda26d4a1755c4076aa66403545ab0b7cd5e
PINNGU <dzonic25@gmail.com>
2025-08-17 23:50:39 +0200

    project init

---
+++ backend/src/main/java/myapp/config/DroolsConfig.java
@@ -0,0 +1,22 @@
+package com.example.socialnetwork.config;
+
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.KieServices;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class DroolsConfig {
+    @Bean
+    public KieContainer kieContainer() {
+        return KieServices.Factory.get().getKieClasspathContainer();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public KieSession kieSession(KieContainer kieContainer) {
+        return kieContainer.newKieSession("ksession-rules");
+    }
+}
